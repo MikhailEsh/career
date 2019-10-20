de8b46e62414dad9505c66f79a6ed49f9ec69ed2
Roccoon <u17@ist.ru>
2019-10-20 13:30:28 +0300

    fix GQL bags

--- src/main/java/com/career/dto/enumDto/StatusEmployeeEnum.java
+++ src/main/java/com/career/dto/enumDto/StatusEmployeeEnum.java
@@ -5,7 +5,8 @@ import com.jraphql.cn.wzvtcsoft.x.bos.domain.CareerEnum;
 public enum StatusEmployeeEnum implements CareerEnum{
 
     WORK_NOW("WORK_NOW","WorkNow"),
-    WORKED("WORKED","Worked");
+    WORKED("WORKED","Worked"),
+    UNKNOWN("UNKNOWN","Unknown");
     private CareerEnum.EnumInnerValue ev;
 
     StatusEmployeeEnum(String value, String name) {
